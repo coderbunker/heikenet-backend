692f15cc4e50dee20007a6ddefea105020152dc3
denoptic <konsistentsi@gmail.com>
2018-10-31 14:11:43 +0800

    ADD: functions for interact with smart contract

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	retainer_contract "github.com/coderbunker/heike-network/contracts/retainer"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"math/big"
@@ -80,38 +81,44 @@ func main() {
 		log.Fatalf("failed to instantiate a contract: %v", err)
 	}
 
-	// "DAI" as bytes
+	// "DAI" as [32]bytes
+	// symbol := "DAI"
+	// var token_symbol [32]byte
+	// copy(token_symbol[:], symbol)
+
+	symbol := "0x686573"
+	symbol_hex, _ := hexutil.Decode(symbol)
 	var token_symbol [32]byte
-	copy(token_symbol[:], "DAI")
-	log.Println(token_symbol)
-	// 0x686573000000000000000000000000000000
-	// z := [32]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 68, 65, 73}
+	copy(token_symbol[:], symbol_hex)
 
 	// add token to retainer
 	// a, err := retainer_instance.AddNewToken(&bind.TransactOpts{
 	// 	From:     auth.From,
 	// 	Signer:   auth.Signer,
-	// 	GasLimit: 24000,
+	// 	GasLimit: 50000,
 	// }, token_symbol, common.HexToAddress(dai))
-	// log.Println("add result:", a)
-	// log.Println("add error:", err)
-	//
-	// b, err := retainer_instance.Tokens(&bind.CallOpts{}, token_symbol)
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
-	// log.Println("tokens result:", b)
+	// log.Println("add result:", a)
+	// log.Println("auth", auth)
+
+	// tokens_res, err := retainer_instance.Tokens(&bind.CallOpts{}, token_symbol)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println("tokens result:", tokens_res)
 
 	// fund retainer
-	fund_res, err := retainer_instance.FundRetainer(&bind.TransactOpts{
-		From:     auth.From,
-		Signer:   auth.Signer,
-		GasLimit: 22000,
-	}, token_symbol, big.NewInt(1000))
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("fund result:", fund_res)
+	// fund_res, err := retainer_instance.FundRetainer(&bind.TransactOpts{
+	// 	From:     auth.From,
+	// 	Signer:   auth.Signer,
+	// 	GasLimit: 300000,
+	// }, token_symbol, big.NewInt(444))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println("fund result:", fund_res)
 
 	balance, err := erc20_instance.BalanceOf(&bind.CallOpts{}, common.HexToAddress(retainer))
 	if err != nil {
@@ -119,10 +126,16 @@ func main() {
 	}
 	log.Printf("retainer balance (wei): %s\n", balance)
 
-	// retainer_instance.WithdrawRetainer(&bind.TransactOpts{
-	// 	From:   auth.From,
-	// 	Signer: auth.Signer,
-	// 	Value:  nil,
-	// }, common.HexToAddress(retainer), big.NewInt(4444))
-	// // symbol to, amount
+	// fl_address := common.HexToAddress("0x5b872ee8aa4ed09b6b92f631f42d3aaf7622b53e")
+	fl_address := common.HexToAddress("0x122e6c2B891C7EB0F31a8191016D19A9663665B9")
+
+	with_res, err := retainer_instance.WithdrawRetainer(&bind.TransactOpts{
+		From:   auth.From,
+		Signer: auth.Signer,
+		Value:  nil,
+	}, token_symbol, fl_address, big.NewInt(100))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("withdr result:", with_res)
 }
