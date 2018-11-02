b8a82ade9b0394dfe194616dd2687d2b70e79cf9
denoptic <konsistentsi@gmail.com>
2018-11-02 15:06:43 +0800

    ADD: get, update, delete user

--- controllers/users.go
+++ controllers/users.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 
+	mid "github.com/coderbunker/heikenet-backend/middleware"
 	"github.com/coderbunker/heikenet-backend/models"
 )
 
@@ -16,7 +19,12 @@ func CreateUser(c echo.Context) error {
 		})
 	}
 
-	user, err := models.CreateUser(c, new_user)
+	db, err := mid.GetDB(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := models.CreateUser(db, new_user)
 	if err != nil {
 		return c.JSON(http.StatusConflict, map[string]string{
 			"error": err.Error(),
@@ -27,28 +35,77 @@ func CreateUser(c echo.Context) error {
 }
 
 func GetUser(c echo.Context) error {
-	// id, err := strconv.Atoi(c.Param("id"))
-	// if err != nil {
-	// 	return c.JSON(http.StatusConflict, map[string]string{
-	// 		"error": "can't read id",
-	// 	})
-	// }
-	//
-	// user, err := models.GetUser(c, id)
-	// if err != nil {
-	// 	return c.JSON(http.StatusConflict, map[string]string{
-	// 		"error": "can't find user",
-	// 	})
-	// } else {
-	// 	return c.JSON(http.StatusCreated, user)
-	// }
-	return c.String(http.StatusOK, "id")
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusConflict, map[string]string{
+			"error": "can't read id",
+		})
+	}
+
+	db, err := mid.GetDB(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := models.GetUser(db, id)
+	if err != nil {
+		return c.JSON(http.StatusConflict, map[string]string{
+			"error": err.Error(),
+		})
+	} else {
+		return c.JSON(http.StatusCreated, user)
+	}
 }
 
 func UpdateUser(c echo.Context) error {
-	return c.String(http.StatusOK, "update")
+	new_user := new(models.NewUser)
+	if err := c.Bind(new_user); err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, map[string]string{
+			"error": "can't read json",
+		})
+	}
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusConflict, map[string]string{
+			"error": "can't read id",
+		})
+	}
+
+	db, err := mid.GetDB(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := models.UpdateUser(db, id, new_user)
+	if err != nil {
+		return c.JSON(http.StatusConflict, map[string]string{
+			"error": err.Error(),
+		})
+	} else {
+		return c.JSON(http.StatusOK, user)
+	}
 }
 
 func DeleteUser(c echo.Context) error {
-	return c.String(http.StatusOK, "delete")
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusConflict, map[string]string{
+			"error": "can't read id",
+		})
+	}
+
+	db, err := mid.GetDB(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = models.DeleteUser(db, id)
+	if err != nil {
+		return c.JSON(http.StatusConflict, map[string]string{
+			"error": err.Error(),
+		})
+	} else {
+		return c.NoContent(http.StatusNoContent)
+	}
 }
--- models/models.go
+++ models/models.go
@@ -3,10 +3,8 @@ package models
 import (
 	"errors"
 
-	"github.com/labstack/echo"
+	"github.com/jinzhu/gorm"
 	"golang.org/x/crypto/bcrypt"
-
-	mid "github.com/coderbunker/heikenet-backend/middleware"
 )
 
 type (
@@ -24,14 +22,9 @@ type (
 	}
 )
 
-func CreateUser(c echo.Context, new_user *NewUser) (*User, error) {
+func CreateUser(db *gorm.DB, new_user *NewUser) (*User, error) {
 	if new_user.Name == "" || new_user.Email == "" || new_user.Password == "" {
 		return nil, errors.New("invalid account details")
-	}
-
-	db, err := mid.GetDB(c)
-	if err != nil {
-		return nil, err
 	}
 
 	var user User
@@ -51,5 +44,50 @@ func CreateUser(c echo.Context, new_user *NewUser) (*User, error) {
 		return &user, nil
 	} else {
 		return nil, errors.New("user exists")
+	}
+}
+
+func GetUser(db *gorm.DB, id int) (*User, error) {
+	var user User
+	if db.First(&user, "id = ?", id).RecordNotFound() {
+		return nil, errors.New("user not found")
+	} else {
+		return &user, nil
+	}
+}
+
+func UpdateUser(db *gorm.DB, id int, new_user *NewUser) (*User, error) {
+	if new_user.Name == "" || new_user.Email == "" || new_user.Password == "" {
+		return nil, errors.New("invalid account details")
+	}
+
+	var user User
+	if db.First(&user, "id = ?", id).RecordNotFound() {
+		return nil, errors.New("user not found")
+	} else {
+		hpassword, err := bcrypt.GenerateFromPassword([]byte(new_user.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return nil, err
+		}
+
+		db.Model(&user).Updates(
+			User{
+				Name:     new_user.Name,
+				Email:    new_user.Email,
+				Password: hpassword,
+			},
+		)
+
+		return &user, nil
+	}
+}
+
+func DeleteUser(db *gorm.DB, id int) error {
+	var user User
+	if db.First(&user, "id = ?", id).RecordNotFound() {
+		return errors.New("user not found")
+	} else {
+		db.Delete(&user)
+		return nil
 	}
 }
